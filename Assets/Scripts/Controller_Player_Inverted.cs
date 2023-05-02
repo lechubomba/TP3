da4cf7a0df7c5a56d918729c6a27f640af2bc588
franco-gabriel <lafuentefrancogabriel@gmail.com>
2023-05-02 19:16:29 -0300

    Codigo de personaje adhesivo implementado

--- Assets/Scripts/Controller_Player_Inverted.cs
+++ Assets/Scripts/Controller_Player_Inverted.cs
@@ -25,4 +25,36 @@ public class Controller_Player_Inverted : Controller_Player
             }
         }
     }
+    /*El siguiente codigo permite que el personaje verde rompa las leyes de la gravedad y se adhiera a cualquier superficie que toque*/
+    /*
+      public override void Jump()
+    {
+        if (IsOnSomething())
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rb.AddForce(new Vector3(0, -jumpForce, 0), ForceMode.Impulse);
+                StartCoroutine(GroundSnap());
+            }
+        }
+    }
+
+    private IEnumerator GroundSnap()
+    {
+        // Wait for a short time to allow the player to leave the ground
+        yield return new WaitForSeconds(0.1f);
+
+        // Check if the player is above an object
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 1.5f))
+        {
+            // Align the player's rotation to the surface normal
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+
+            // Snap the player to the surface
+            transform.position = hit.point + hit.normal * 0.5f;
+        }
+    }
+    
+     */
 }
