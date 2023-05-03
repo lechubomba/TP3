406fc67f93b249797fced0e3407bcad33b365c85
franco-gabriel <lafuentefrancogabriel@gmail.com>
2023-05-02 21:03:56 -0300

    Commit 4
    Personaje que rebota en los techos mejorado(tiene algunos bugs)

--- Assets/Scripts/Controller_Player_Inverted.cs
+++ Assets/Scripts/Controller_Player_Inverted.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class Controller_Player_Inverted : Controller_Player
 {
+    private Vector3 surfaceNormal = Vector3.up;
+
     public override void FixedUpdate()
     {
-        base.rb.AddForce(new Vector3(0, 30f, 0));
         base.FixedUpdate();
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 1f))
+        {
+            // Align the player's rotation to the surface normal
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+
+            // Snap the player to the surface
+            transform.position = hit.normal + hit.point * 0.5f;
+        }
     }
+
 
     public override bool IsOnSomething()
     {
-        return Physics.BoxCast(transform.position, new Vector3(transform.localScale.x * 0.9f, transform.localScale.y / 3, transform.localScale.z * 0.9f), Vector3.up, out downHit, Quaternion.identity, downDistanceRay);
+        bool isOnSomething = Physics.BoxCast(transform.position, new Vector3(transform.localScale.x * 0.9f, transform.localScale.y / 3, transform.localScale.z * 0.9f), Vector3.up, out downHit, Quaternion.identity, downDistanceRay);
+
+        if (isOnSomething)
+        {
+            surfaceNormal = downHit.normal;
+        }
+
+        return isOnSomething;
     }
+
 
     public override void Jump()
     {
@@ -21,10 +41,11 @@ public class Controller_Player_Inverted : Controller_Player
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
-                rb.AddForce(new Vector3(0, -jumpForce, 0), ForceMode.Impulse);
+                rb.AddForce(surfaceNormal * jumpForce, ForceMode.Impulse);
             }
         }
     }
+
     /*El siguiente codigo permite que el personaje verde rompa las leyes de la gravedad y se adhiera a cualquier superficie que toque*/
     /*
       public override void Jump()
