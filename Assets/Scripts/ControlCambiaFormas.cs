205d03fc87db1b7841b31475a73c6fa41bcbe918
franco-gabriel <lafuentefrancogabriel@gmail.com>
2023-05-09 23:52:57 -0300

    Commit 11
    Personaje que cambia de formas terminado

--- Assets/Scripts/ControlCambiaFormas.cs
+++ Assets/Scripts/ControlCambiaFormas.cs
@@ -15,67 +15,20 @@ public class ControlCambiaFormas : MonoBehaviour
     public GameObject ladderModel;
     public float transformTime = 1.0f;
 
-    private bool isTransformed = false;
-    private bool hasTransformed = false;
-    private float currentTransformTime = 0.0f;
-    private Vector3 originalScale;
+    
 
     void Start()
     {
-        originalScale = Player2.transform.localScale;
+        
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T) && !hasTransformed)
-        {
-            if (!isTransformed)
-            {
-                StartCoroutine(TransformToLadder());
-            }
-            else
-            {
-                StartCoroutine(TransformToPlayer());
-            }
-            hasTransformed = true;
-        }
+       
     }
 
-    IEnumerator TransformToLadder()
-    {
-        currentTransformTime = 0.0f;
-
-        while (currentTransformTime < transformTime)
-        {
-            currentTransformTime += Time.deltaTime;
-            float t = currentTransformTime / transformTime;
-            Player2.transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, t);
-            ladderModel.transform.localScale = Vector3.Lerp(Vector3.zero, originalScale, t);
-            yield return null;
-        }
-
-        Player2.SetActive(false);
-        ladderModel.SetActive(true);
-        isTransformed = true;
-    }
-
-    IEnumerator TransformToPlayer()
-    {
-        currentTransformTime = 0.0f;
-
-        while (currentTransformTime < transformTime)
-        {
-            currentTransformTime += Time.deltaTime;
-            float t = currentTransformTime / transformTime;
-            Player2.transform.localScale = Vector3.Lerp(Vector3.zero, originalScale, t);
-            ladderModel.transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, t);
-            yield return null;
-        }
-
-        Player2.SetActive(true);
-        ladderModel.SetActive(false);
-        isTransformed = false;
-    }
+    
+    
 
     void FixedUpdate()
     {
@@ -99,67 +52,3 @@ public class ControlCambiaFormas : MonoBehaviour
         }
     }
 }
-/*
-public GameObject Player2;
-public GameObject ladderModel;
-public float transformTime = 1.0f;
-
-private bool isTransformed = false;
-private float currentTransformTime = 0.0f;
-private Vector3 originalScale;
-
-void Start()
-{
-    originalScale = playerModel.transform.localScale;
-}
-
-void Update()
-{
-    if (Input.GetKeyDown(KeyCode.T))
-    {
-        if (!isTransformed)
-        {
-            StartCoroutine(TransformToLadder());
-        }
-        else
-        {
-            StartCoroutine(TransformToPlayer());
-        }
-    }
-}
-
-IEnumerator TransformToLadder()
-{
-    currentTransformTime = 0.0f;
-
-    while (currentTransformTime < transformTime)
-    {
-        currentTransformTime += Time.deltaTime;
-        float t = currentTransformTime / transformTime;
-        playerModel.transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, t);
-        ladderModel.transform.localScale = Vector3.Lerp(Vector3.zero, originalScale, t);
-        yield return null;
-    }
-
-    playerModel.SetActive(false);
-    ladderModel.SetActive(true);
-    isTransformed = true;
-}
-
-IEnumerator TransformToPlayer()
-{
-    currentTransformTime = 0.0f;
-
-    while (currentTransformTime < transformTime)
-    {
-        currentTransformTime += Time.deltaTime;
-        float t = currentTransformTime / transformTime;
-        playerModel.transform.localScale = Vector3.Lerp(Vector3.zero, originalScale, t);
-        ladderModel.transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, t);
-        yield return null;
-    }
-
-    playerModel.SetActive(true);
-    ladderModel.SetActive(false);
-    isTransformed = false;
-}*/
